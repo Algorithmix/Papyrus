46d89f5389ec37022c6016608598a37621de7cfc
Jeffrey Crowell <crowell@bu.edu>
2012-09-25 01:03:23 -0400

    Added FloodFill and sample project
    Added a new sample project with a flood fill example.
    Added the flood fill code with the sample output.
    Fixed the Picasso Boundary checking code (fencepost error)

--- Picasso/Picasso/Picasso.cs
+++ Picasso/Picasso/Picasso.cs
@@ -29,10 +29,58 @@ namespace Picasso
             return Math.Sqrt((double)(red * red + green * green + blue * blue));
         }
 
+        /// <summary>
+        /// Flood fill in a BFS manner, so as not to overwhelm the stack
+        /// </summary>
+        /// <param name="image">the image we wish to fill on</param>
+        /// <param name="xpixel">the x pixel to sample from</param>
+        /// <param name="ypixel">the y pixel to sample from</param>
+        /// <param name="threshold">the threshold of difference</param>
+        /// <returns>the background which can be subtracted</returns>
+        public static Bitmap FloodFill(Bitmap image, int xpixel, int ypixel, double threshold)
+        {
+            Color target = image.GetPixel(xpixel, ypixel);
+            //create an identically sized "background" image and fill it white
+            Bitmap background = new Bitmap(image.Width, image.Height);
+            for(int ii = 0; ii < image.Width; ii++)
+            {
+                for (int jj = 0; jj < image.Height; jj++)
+                {
+                    background.SetPixel(ii, jj, Color.White);
+                }
+            }
+            Queue<Point> pointQueue = new Queue<Point>();
+            pointQueue.Enqueue(new Point(xpixel, ypixel));
+            while (!(pointQueue.Count == 0)) //make sure queue isn't empty
+            {
+                Point p = pointQueue.Dequeue();
+                //add all neighboring points to the a list
+                List<Point> pList = new List<Point>();
+                pList.Add(new Point(p.X, p.Y - 1)); //above
+                pList.Add(new Point(p.X, p.Y + 1)); //below
+                pList.Add(new Point(p.X - 1, p.Y)); //left
+                pList.Add(new Point(p.X + 1, p.Y)); //right
+                foreach (Point neighbor in pList)
+                {
+                    if(!(Bound(image, neighbor.X, neighbor.Y)))
+                        continue;
+                    double dist = Distance(image.GetPixel(neighbor.X, neighbor.Y), target);
+                    Color color = background.GetPixel(neighbor.X, neighbor.Y);
+                    if ((dist < threshold) && (Distance(color, Color.White) < 5)) //and hasn't been seen before
+                    {
+                        background.SetPixel(neighbor.X, neighbor.Y, Color.Gray); //set it as added to the queue
+                        pointQueue.Enqueue(neighbor); //and add to the queue
+                    }
+                }
+                background.SetPixel(p.X, p.Y, Color.HotPink); //set the pixel to hot pink
+            }
+            return background;
+        }
+
 
         public static bool Bound(Bitmap image, int xx, int yy)
         {
-            if ( xx > 0 && xx < image.Width && yy>0 && yy<image.Height )
+            if ( xx >= 0 && xx < image.Width && yy >= 0 && yy < image.Height )
             {
                 return true;
             }
---
+++ Picasso/PicassoSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Picasso;
+using System.Drawing;
+
+namespace PicassoSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap source = new Bitmap(args[0]);
+            Bitmap mask = Utility.FloodFill(source, 0, 0, 150);
+            mask.Save("mask.png");
+        }
+    }
+}
