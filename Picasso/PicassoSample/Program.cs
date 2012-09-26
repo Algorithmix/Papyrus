a2ccf1bf3b35afa77ed6259afc22d2ac36d0ac60
Samir ahmed <samirahmed2013@gmail.com>
2012-09-26 01:10:35 -0400

    Added Test for FloodFill
    - Added Managed EMGU References to the Test CSPROJ
    
    - Explicitly made build targets x64, for use with the
    CvInvoke Unmanaged Linked DLLs.
    
    - Added Test for the Flood Fill method
    
    - Added Image Previewing in the Sample so that you can view the
    image directly without having to open it in a new dir

--- Picasso/PicassoSample/Program.cs
+++ Picasso/PicassoSample/Program.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Picasso;
 using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.UI;
 
 namespace PicassoSample
 {
@@ -15,6 +19,15 @@ namespace PicassoSample
             string imagesrc = args[0];
             Bitmap source = new Bitmap(imagesrc);
             Bitmap mask = Utility.FloodFill(source, 0, 0, 95);
+
+            // Display to the User
+            var result = new Image<Bgr, Byte>(mask);
+            ImageViewer display = new ImageViewer(result, "Mask");
+            var scale = Math.Min(800.0 / (double)result.Height, 800.0 / (double)result.Width);
+            display.ImageBox.SetZoomScale(scale, new Point(10, 10));
+            display.ShowDialog();
+
+            // Save to Working Dir
             mask.Save("mask.png");
         }
     }
