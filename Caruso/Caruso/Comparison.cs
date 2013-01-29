8bf5fe720bf0c07df0fe0419a3f6b6b6449e38c3
Samir ahmed <samirahmed2013@gmail.com>
2013-01-29 12:49:30 -0500

    Added Comparison Class
    This is intended to hold all the data for comparison.
    
    However the implementation will probably need to be changed
    to a struct

---
+++ Caruso/Caruso/Comparison.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithmix
+{
+    public class EdgeData
+    {
+        
+        public long Id;
+        public Direction Direction;
+        public Orientation Orientation;
+        
+        public EdgeData( long id, Direction direction, Orientation orientation)
+        {
+            this.Id = id;
+            this.Direction = direction;
+            this.Orientation = orientation;
+        }
+    }
+
+    public class Comparison
+    {
+        public readonly EdgeData First;
+        public readonly EdgeData Second;
+        public readonly double Similarity;
+        public readonly int Offset;
+
+        public Comparison(double chamferSimilarity, int offset, EdgeData first, EdgeData second)
+        {
+            this.First = first;
+            this.Second = second;
+            this.Similarity = chamferSimilarity;
+            this.Offset = offset;
+        }
+    }
+}
