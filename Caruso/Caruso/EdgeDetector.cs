320876fe2920dcc0c63c32bed218ea0976d09e03
Richard Tia <richie.tia@gmail.com>
2013-02-02 21:49:18 -0500

    Edge Detector
    Added Edge Detector Tests to Caruso

---
+++ Caruso/Caruso/EdgeDetector.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using AForge.Imaging.Filters;
+
+namespace Algorithmix
+{
+    namespace Forensics
+    {
+        public class EdgeDetector
+        {
+            public static Tuple<double, double> AnalyzeShred(Bitmap inShred)
+            {
+                Bitmap gsShred = Grayscale.CommonAlgorithms.BT709.Apply(inShred);
+                CannyEdgeDetector filter = new CannyEdgeDetector();
+                Bitmap shred = filter.Apply(gsShred);
+
+                double lMean = 0;
+                double rMean = 0;
+
+                double lrunsum = 0;
+                double rrunsum = 0;
+
+                int count = 0;
+
+                Queue<int> lData = new Queue<int>();
+                Queue<int> rData = new Queue<int>();
+
+                for (int j = (int)(shred.Height * 0.25); j < (int)(shred.Height * 0.75); j++)
+                {
+                    if (lData.Count == 10)
+                    {
+                        lMean = getAverage(lData);
+                        rMean = getAverage(rData);
+                    }
+                    ArrayList xHits = new ArrayList();
+
+                    //traverse each row to record edge location
+                    for (int i = 0; i < shred.Width; i++)
+                    {
+                        if (shred.GetPixel(i, j).B >= 50)
+                        {
+                            xHits.Add(i);
+                        }
+                    }
+
+                    int currentLowest = 99999;
+                    int currentHighest = 0;
+
+                    //abstract relevent edges from xHits
+                    if (xHits.Count >= 2)
+                    {
+                        foreach (int x in xHits)
+                        {
+                            if (x < currentLowest)
+                            {
+                                currentLowest = x;
+                            }
+                            if (x > currentHighest)
+                            {
+                                currentHighest = x;
+                            }
+                        }
+
+
+                        //add data to queue's
+                        if (lData.Count < 10)
+                        {
+                            lData.Enqueue(currentLowest);
+                            rData.Enqueue(currentHighest);
+                        }
+                        else
+                        {
+                            lData.Enqueue(currentLowest);
+                            rData.Enqueue(currentHighest);
+
+                            lData.Dequeue();
+                            rData.Dequeue();
+                        }
+
+                        //record distance from mean squared (variance)
+                        if (lMean > 0)
+                        {
+                            lrunsum += (lMean - currentLowest) * (lMean - currentLowest);
+                            rrunsum += (rMean - currentHighest) * (rMean - currentHighest);
+                            count++;
+                        }
+                    }
+                }
+
+                double lVariance = lrunsum / count;
+                double rVariance = rrunsum / count;
+
+                Tuple<double, double> output = new Tuple<double, double>(lVariance, rVariance);
+
+                return output;
+            }
+
+
+            public static double getAverage(Queue<int> input)
+            {
+                int runsum = 0;
+
+                foreach (int x in input)
+                {
+                    runsum += x;
+                }
+
+                return runsum / (double)input.Count;
+            }
+        }
+    }
+}
---
+++ Caruso/CarusoTest/EdgeDetectorTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Algorithmix;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithmix.Forensics;
+using System.Drawing;
+using Algorithmix.TestTools;
+
+
+namespace CarusoTest
+{
+    [TestClass]
+    public class EdgeDetectorTest
+    {
+        private static string EdgeDetectorDirectory = "EdgeDetectorTest";
+        [TestMethod]
+        public void AnalyzeShredTest()
+        {
+            var path = Path.Combine(Helpers.CarusoTestDirectory, EdgeDetectorDirectory);
+            var drive = new Drive(path, Drive.Reason.Read);
+
+            var list = drive.GetAllMatching("image");
+
+            double leftEdge = 100;
+            double rightEdge = 100;
+
+            string leftEdgeFile = "";
+            string rightEdgeFile = "";
+            
+            foreach (var filename in list)
+            {
+                Bitmap bitmap = new Bitmap(filename);
+                var variance = Algorithmix.Forensics.EdgeDetector.AnalyzeShred(bitmap);
+                Console.WriteLine("Left: " + variance.Item1 + " Right: " + variance.Item2 + " | " + filename);
+
+                if (variance.Item1 < leftEdge)
+                {
+                    leftEdge = (double)variance.Item1;
+                    leftEdgeFile = filename;
+                }
+                    
+                if (variance.Item2 < rightEdge)
+                {
+                    rightEdge = (double)variance.Item2;
+                    rightEdgeFile = filename;
+                }
+                    
+            }
+
+            if (leftEdgeFile != string.Empty)
+            {
+                Console.WriteLine("Lowest left edge variance: " + leftEdge + " | " + leftEdgeFile);
+            }
+            if (rightEdgeFile != string.Empty)
+            {
+                Console.WriteLine("Lowest right edge variance: " + rightEdge + " | " + rightEdgeFile);
+            }
+
+
+        }
+
+        [TestMethod]
+        public void GetAverageTest()
+        {
+            Queue<int> testQueue = new Queue<int>();
+            var total = 0;
+            var count = 400;
+            for (int ii = 1; ii <= count; ii++)
+            {
+                total += ii;
+                testQueue.Enqueue(ii);
+            }
+
+            var expected = (double)total/count;
+            var actual = EdgeDetector.getAverage(testQueue);
+            Assert.IsTrue(Math.Abs(expected - actual) < 0.001);
+        }
+    }
+    
+}
+
+
