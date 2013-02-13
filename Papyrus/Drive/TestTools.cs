a2f746185a76ee88978e88b81c98c51cfc87196e
Samir ahmed <samirahmed2013@gmail.com>
2013-02-13 01:48:36 -0500

    copy drive into papyrus

---
+++ Papyrus/Drive/TestTools.cs
@@ -0,0 +1,168 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+
+#endregion
+
+namespace Algorithmix
+{
+    namespace TestTools
+    {
+        public class TestDriveException : Exception
+        {
+            public TestDriveException()
+            {
+            }
+
+            public TestDriveException(string message)
+                : base(message)
+            {
+            }
+
+            public TestDriveException(string message, Exception inner)
+                : base(message, inner)
+            {
+            }
+
+            protected TestDriveException(SerializationInfo info,
+                                         StreamingContext context)
+            {
+            }
+        }
+
+        public class Drive
+        {
+            #region Reason enum
+
+            public enum Reason
+            {
+                Read,
+                Save
+            }
+
+            #endregion
+
+            private static string TESTDRIVE_ROOT;
+            public static int MAX_DEPTH = 10;
+            private readonly string _directory;
+            private readonly Reason _reason;
+
+            public Drive(string relativePath, Reason reason)
+            {
+                var directory = Path.Combine(GetDriveRoot(), relativePath);
+                if (!Directory.Exists(directory) && reason == Reason.Read)
+                {
+                    throw new TestDriveException("no such directory: " + directory);
+                }
+                else if (Directory.Exists(directory) && reason == Reason.Save)
+                {
+                    throw new TestDriveException("Attempting to open an existing directory: " + directory);
+                }
+                else if (!Directory.Exists(directory) && reason == Reason.Save)
+                {
+                    Directory.CreateDirectory(directory);
+                }
+                _reason = reason;
+                _directory = directory;
+            }
+
+            public static string GetDriveRoot()
+            {
+                if (TESTDRIVE_ROOT == null)
+                {
+                    TESTDRIVE_ROOT = Environment.GetEnvironmentVariable("TESTDRIVE_ROOT");
+                    if (TESTDRIVE_ROOT == null)
+                    {
+                        throw new TestDriveException(
+                            "TESTDRIVE_ROOT Environment Variable not found - please ensure you have it set");
+                    }
+                }
+                return TESTDRIVE_ROOT;
+            }
+
+            public IEnumerable<String> FullFilePaths()
+            {
+                return Files().Select(file => Path.Combine(_directory, file));
+            }
+
+            public IEnumerable<String> Files()
+            {
+                return Directory.EnumerateFiles(_directory);
+            }
+
+            public IEnumerable<String> Files(string prefix)
+            {
+                return
+                    Directory.EnumerateFiles(_directory).Where(
+                        filename => filename.StartsWith(Path.Combine(_directory, prefix)));
+            }
+
+            public int FileCount()
+            {
+                return Files().Count();
+            }
+
+            public int FileCount(string prefix)
+            {
+                return Files(prefix).Count();
+            }
+
+            public void Save(Bitmap bitmap, string prefix)
+            {
+                if (_reason == Reason.Save)
+                {
+                    bitmap.Save(Path.Combine(_directory, prefix + "_" + (FileCount(prefix) + 1)), ImageFormat.Jpeg);
+                }
+                else
+                {
+                    throw new TestDriveException("Attempted to Save File in Read Only Directory");
+                }
+            }
+
+            public void Save(IEnumerable<Bitmap> bitmaps, string prefix)
+            {
+                foreach (Bitmap bitmap in bitmaps)
+                {
+                    Save(bitmap, prefix);
+                }
+            }
+
+            public List<string> GetAllMatching(string prefix)
+            {
+                var files = new List<string>();
+                GetAllMatching(prefix, _directory, files);
+                return files;
+            }
+
+            /// <summary>
+            ///   Given a root directory, recursiveky search through subfolders searching for shred images
+            /// </summary>
+            /// <param name="targetDirectory"> </param>
+            /// <param name="myIndexDictionary"> </param>
+            /// <param name="textFilePath"> </param>
+            private void GetAllMatching(string prefix, string directory, List<string> files, int depth = 0)
+            {
+                if (depth >= MAX_DEPTH)
+                {
+                    return;
+                }
+                // Process the list of files found in the directory.
+                Directory.EnumerateFiles(directory)
+                    .Where(filename => filename.StartsWith(Path.Combine(directory, prefix)))
+                    .ToList().ForEach(filename => files.Add(filename));
+
+                // Recurse into subdirectories of this directory. 
+                foreach (string subdirectory in Directory.GetDirectories(directory))
+                {
+                    GetAllMatching(prefix, subdirectory, files);
+                }
+            }
+        }
+    }
+}
---
+++ Papyrus/DriveTest/tests.cs
@@ -0,0 +1,111 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Algorithmix.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        public readonly string UnitTestFolder = "TestDriveUnitTest";
+        public readonly string TestMaterialFolder = "RawShreds";
+
+        [TestMethod]
+        public void GetDriveRootTest()
+        {
+            Assert.IsNotNull(Drive.GetDriveRoot());
+        }
+
+        [TestMethod]
+        public void TestSaveAndLoad()
+        {
+            const string saveDirectory = "TestSaving";
+            RemoveDirectory(Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, saveDirectory));
+            var saveFolder = new Drive(Path.Combine(UnitTestFolder, saveDirectory), Drive.Reason.Save);
+            Assert.IsTrue(saveFolder.FileCount() == 0);
+
+            var loadFolder = new Drive(Path.Combine(UnitTestFolder, TestMaterialFolder), Drive.Reason.Read);
+            var totalFileCount = loadFolder.FileCount();
+            Assert.IsTrue(totalFileCount > 0);
+
+            var bitmaps = new List<Bitmap>(totalFileCount);
+            foreach (string filepath in loadFolder.FullFilePaths())
+            {
+                var bitmap = new Bitmap(filepath);
+                bitmaps.Add(bitmap);
+            }
+            Assert.IsTrue(bitmaps.Count == totalFileCount);
+
+            saveFolder.Save(bitmaps, "unittest");
+            Assert.IsTrue(saveFolder.FileCount("unittest") == totalFileCount);
+            Assert.IsTrue(saveFolder.FileCount() == totalFileCount);
+        }
+
+        [TestMethod]
+        public void TestExceptions()
+        {
+        }
+
+        [TestMethod]
+        public void TestGetAllMatching()
+        {
+            const string dir = "TestRecursiveMatching";
+            RemoveDirectory(Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, dir));
+            var expected = new List<string>();
+            BuildNestedDirectory(Path.Combine(Drive.GetDriveRoot(), UnitTestFolder, dir), expected);
+            var drive = new Drive(Path.Combine(UnitTestFolder, dir), Drive.Reason.Read);
+            var actual = drive.GetAllMatching("image");
+            expected.Sort();
+            actual.Sort();
+            var same = expected.Zip(actual, (first, second) => first == second).All(x => x);
+            Assert.IsTrue(same);
+        }
+
+        private void BuildNestedDirectory(string dir, List<string> filenames, int size = 4)
+        {
+            if (size <= 0)
+            {
+                return;
+            }
+            Directory.CreateDirectory(dir);
+            var file1 = Path.Combine(dir, "image_1" + size + ".jpg");
+            var file2 = Path.Combine(dir, "image_2" + size + ".jpg");
+            filenames.Add(file1);
+            filenames.Add(file2);
+
+            File.Create(file1).Dispose();
+            File.Create(file2).Dispose();
+            File.Create(Path.Combine(dir, "banana.jpg")).Dispose();
+
+            var newdir1 = Path.Combine(dir, "fruits");
+            var newdir2 = Path.Combine(dir, "vegetables");
+            BuildNestedDirectory(newdir1, filenames, size - 1);
+            BuildNestedDirectory(newdir2, filenames, size - 1);
+        }
+
+        private void RemoveDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+            var files = Directory.EnumerateFiles(path);
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                RemoveDirectory(dir);
+            }
+            Directory.Delete(path);
+        }
+    }
+}
