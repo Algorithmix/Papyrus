f596e0c2a9b8427105e353900db32c930388aeb4
Samir ahmed <samirahmed2013@gmail.com>
2013-02-13 00:48:26 -0500

    Added Picasso

---
+++ Caruso/CarusoTest/HeuristicsTest.cs
@@ -0,0 +1,41 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Picasso;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PicassoTest
+{
+    [TestClass]
+    public class HeuristicsTest
+    {
+        [TestMethod]
+        public void DetectBackgroundTest()
+        {
+            // Create a Square
+            Point[] shred = new Point[4];
+            shred[0] = new Point(0,0);
+            shred[1] = new Point(0, 99);
+            shred[2] = new Point(10, 99);
+            shred[3] = new Point(10, 0);
+
+            // Create an Original Image
+            var original = new Image<Bgr, Byte>(100, 100, new Bgr(Color.HotPink));
+            original.FillConvexPoly(shred, new Bgr(Color.Gray));
+
+            var expected = new Bgr(Color.HotPink);
+
+            Console.WriteLine("Performing Heuristic Background Detection");
+            var actual = Heuristics.DetectBackground(original.ToBitmap());
+            Assert.IsTrue( Picasso.Utility.IsEqual(expected,actual) );
+
+        }
+    }
+}
