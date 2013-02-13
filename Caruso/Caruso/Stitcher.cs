23e51baecc87acd0a55b149391591ec7ae3e4932
Samir ahmed <samirahmed2013@gmail.com>
2013-02-13 00:30:07 -0500

    Consolidate Jigsaw and Caruso

---
+++ Caruso/Caruso/Stitcher.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithmix
+{
+    public class Stitcher
+    {
+        public static void ExportImage(Cluster cluster, string path = "cluster.png")
+        {
+            Merge(cluster).Save(path, ImageFormat.Png);
+        }
+
+        public static Bitmap Merge(Cluster cluster)
+        {
+            return Merge(cluster.Flattened);
+        }
+
+        public static Bitmap Merge(IEnumerable<Shred> shreds)
+        {
+            Bitmap[] images = new Bitmap[shreds.Count()];
+            int index = 0;
+            foreach (Shred shred in shreds)
+            {
+                images[index] = shred.Bitmap;
+                index += 1;
+            }
+            return Combine(images);
+        }
+
+        public static Bitmap Combine(Bitmap[] images)
+        {
+            Bitmap finalImage = null;
+            try
+            {
+                int width = 0;
+                int height = 0;
+
+                foreach (Bitmap bitmap in images)
+                {
+                    //update the size of the final bitmap
+                    width += bitmap.Width;
+                    height = bitmap.Height > height ? bitmap.Height : height;
+                }
+
+                //create a bitmap to hold the combined image
+                finalImage = new Bitmap(width, height);
+
+                //get a graphics object from the image so we can draw on it
+                using (Graphics g = Graphics.FromImage(finalImage))
+                {
+                    //set background color
+                    g.Clear(Color.Transparent);
+
+                    //go through each image and draw it on the final image
+                    int offset = 0;
+                    foreach (Bitmap image in images)
+                    {
+                        g.DrawImage(image,
+                                    new Rectangle(offset, 0, image.Width, image.Height));
+                        offset += image.Width;
+                    }
+                }
+
+                return finalImage;
+            }
+            catch (Exception ex)
+            {
+                if (finalImage != null)
+                    finalImage.Dispose();
+
+                throw ex;
+            }
+            finally
+            {
+                //clean up memory
+                foreach (Bitmap image in images)
+                {
+                    image.Dispose();
+                }
+            }
+        }
+    }
+}
---
+++ Caruso/CarusoTest/ReconstructorTests.cs
@@ -0,0 +1,132 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Algorithmix.TestTools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace Algorithmix.UnitTest
+{
+    [TestClass]
+    public class NaiveKruskalTests
+    {
+        public static readonly string NaiveThree = "Three";
+        public static readonly string NaiveSix = "Six";
+        public static readonly string NaiveTen = "Ten";
+
+        [TestMethod]
+        public void NaiveKruskalPrimitive()
+        {
+            Shred.BUFFER = 0;
+            Shred.SAMPLE_SIZE = 1;
+
+            var folder = Path.Combine(Drive.GetDriveRoot(), Dir.NaiveKruskalTestDirectory);
+            var paths = new List<string>();
+            paths.Add(Path.Combine(folder, NaiveThree));
+            paths.Add(Path.Combine(folder, NaiveSix ));
+            paths.Add(Path.Combine(folder, NaiveTen ));
+
+            foreach (var path in paths)
+            {
+                var shreds = Shred.Factory("Shred", path, false);
+                var checker = Helpers.BuildChecker(Path.Combine( path , Helpers.CheckFile) );
+                
+                var results = Reconstructor.NaiveKruskalAlgorithm(shreds);
+
+                var indicies = results.Select((t, pos) => new Tuple<int, Shred>(pos, t)).ToList();
+
+                Console.WriteLine(" ---- "+ path +"----");
+                var result = indicies.Select(pair =>
+                    {
+                        var filename = Path.GetFileName(pair.Item2.Filepath);
+                        Assert.IsNotNull(filename);
+                        Assert.IsNotNull(checker[filename]);
+                        var expected = checker[filename];
+                        var actual = pair.Item1;
+                        Console.WriteLine("Actual " + actual + " vs. "+ expected + " | File: " + filename );
+                        return actual.ToString(CultureInfo.InvariantCulture) == expected;
+                    }).ToList();
+                ClusterExporter.ExportJson(results.First().Root());
+                Console.WriteLine();
+                result.ForEach( Assert.IsTrue);
+            }
+        }
+
+        [TestMethod]
+        public void NaiveKruskalArtificial()
+        {
+            Shred.BUFFER = 0;
+            Shred.SAMPLE_SIZE = 4;
+
+            var path = Path.Combine(Drive.GetDriveRoot(), Dir.ArtificialTestDirectory,Dir.NaiveKruskalHttpDocument);
+            var shreds = Shred.Factory("image", path, false);
+            var results = Reconstructor.NaiveKruskalAlgorithm(shreds);
+
+            var checker = Helpers.BuildChecker(Path.Combine(path, Helpers.CheckFile));
+
+            shreds.ForEach(shred => Console.Write(" " + shred.Id + ", "));
+            Console.WriteLine();
+            results.ForEach(shred => Console.Write(" " + shred.Id + ", "));
+            Console.WriteLine();
+
+            var indicies = results.Select((t, pos) => new Tuple<int, Shred>(pos, t)).ToList();
+            var result = indicies.Select(pair =>
+            {
+                var filename = Path.GetFileName(pair.Item2.Filepath);
+                Assert.IsNotNull(filename);
+                Assert.IsNotNull(checker[filename]);
+                var expected = checker[filename];
+                var actual = pair.Item1;
+                Console.WriteLine("Actual " + actual + " vs. " + expected + " | File: " + filename);
+                return actual.ToString(CultureInfo.InvariantCulture) == expected;
+            }).ToList();
+
+//            var diff = Differ.DiffShredByOrder(results.Select(shred => shred.Id).ToList(),
+//Enumerable.Range(0, results.Count).Select(ii => (long)ii).ToList());
+//            Console.WriteLine("Difference : " + diff);
+            ExportResult((Cluster) shreds.First().Root() ,"../../visualizer/NaiveKruskalArtifical.png");
+        }
+
+        [TestMethod]
+        public void NaiveKruskalAuthentic()
+        {
+            var path = Path.Combine(Drive.GetDriveRoot(), Dir.PdfRequiremnetTestDirectory,
+                                    Dir.PdfRequiremnetTestFullOne);
+            var shreds = Shred.Factory("image", path, false);
+            var results = Reconstructor.NaiveKruskalAlgorithm(shreds);
+            shreds.ForEach(shred => Console.Write(" " + shred.Id + ", "));
+            Console.WriteLine();
+            results.ForEach(shred => Console.Write(" " + shred.Id + ", "));
+            Console.WriteLine();
+            Helpers.PrintTree(results.First().Root());
+        }
+
+        private static void ExportResult(Cluster root, string imageName = "out.png", string path=@"../../visualizer/data.js")
+        {
+            try
+            {
+                ClusterExporter.ExportJson(root,path);
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("JSON Export Failed");
+                Console.WriteLine(ee.ToString());
+            }
+            try
+            {
+                Stitcher.ExportImage(root, imageName);
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("Image Export Failed");
+                Console.WriteLine(ee.ToString());
+            }
+        }
+
+    }
+}
