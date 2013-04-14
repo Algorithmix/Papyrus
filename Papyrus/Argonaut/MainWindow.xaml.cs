857494e34c892262623adce4f1c5b1df1a77e2f0
Jeffrey Crowell <crowell@bu.edu>
2013-04-14 17:48:02 -0400

    make threading

--- Papyrus/Argonaut/MainWindow.xaml.cs
+++ Papyrus/Argonaut/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,6 +29,7 @@ using Color = System.Windows.Media.Color;
 using Path = System.IO.Path;
 using Point = System.Windows.Point;
 
+
 namespace Argonaut
 {
     /// <summary>
@@ -36,8 +38,7 @@ namespace Argonaut
     public partial class MainWindow : MetroWindow
     {
         TextWriter _writer = null;
-        private BackgroundWorker bwPreprocess;
-        private BackgroundWorker bwReconstructor;
+        private Thread WorkerThread;
         private string outPath;
         private int thresh;
         private string inputFile;
@@ -95,9 +96,9 @@ namespace Argonaut
             thresh = (int) sliderFill.Value;
             inputFile = inFile;
             bReconstruct.IsEnabled = false;
-            //bwPreprocess.RunWorkerAsync();
-            Workers.Preprocess_Final(inputFile, outPath, thresh);
-            Workers.Reconstruct("image", outPath, false);
+            WorkerThread.Start();
+            //Workers.Preprocess_Final(inputFile, outPath, thresh);
+            //Workers.Reconstruct("image", outPath, false);
         }
 
         private void fArgonaut_Loaded(object sender, RoutedEventArgs e)
@@ -105,40 +106,17 @@ namespace Argonaut
             // Instantiate the writer
             _writer = new TextBoxStreamWriter(tbLog);
             // Redirect the out Console stream
-            Console.SetOut(_writer);
+           // Console.SetOut(_writer);
 
             Console.WriteLine("Now redirecting output to the text box");
-            bwPreprocess = new BackgroundWorker();
-            bwPreprocess.DoWork += bwPreprocess_DoWork;
-            bwPreprocess.RunWorkerCompleted += bwPreprocess_RunWorkerCompleted;
-            bwReconstructor = new BackgroundWorker();
-            bwReconstructor.DoWork += bwReconstructor_DoWork;
-            bwReconstructor.RunWorkerCompleted += bwReconstructor_RunWorkerCompleted;
+            WorkerThread = new Thread(new ThreadStart(Do_Work));
         }
 
-        private void bwPreprocess_DoWork(object sender, DoWorkEventArgs e)
+        private void Do_Work()
         {
-            tbLog.Text = "LOLDONGS";
-            bReconstruct.IsEnabled = false;
-            WebHandle.Source = new Uri("http://bing.com/");
             Workers.Preprocess_Final(inputFile, outPath, thresh);
-        }
-
-        private void bwPreprocess_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
-            //bwReconstructor.RunWorkerAsync();
-            bReconstruct.IsEnabled = true;
-            
-        }
-        private void bwReconstructor_DoWork(object sender, DoWorkEventArgs e)
-        {
-            Workers.Reconstruct("image", outPath, false);        
-        }
-
-        private void bwReconstructor_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
-        {
+            Workers.Reconstruct("image", outPath, false);
             bReconstruct.IsEnabled = true;
         }
-
     }
 }
